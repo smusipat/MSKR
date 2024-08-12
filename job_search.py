9cac0d992381c7310e9bcf6807afe82852ea3dca
smusipat <mskrmanthani@gmail.com>
2024-08-12 19:19:33 -0400

    city name change

--- job_search.py
+++ job_search.py
@@ -15,7 +15,7 @@ def search_google_jobs(role, location=None):
     return job_links
 
 role = "Product Manager"
-location = "pittsburgh"
+location = "hartford"
 job_links = search_google_jobs(role, location)
 for link in job_links:
     print(link)
