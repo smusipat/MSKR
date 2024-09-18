33663d0348467146879f12c3961dab15261fcbd4
smusipat <mskrmanthani@gmail.com>
2024-09-17 21:01:49 -0400

    comment

--- Bandname.py
+++ Bandname.py
@@ -7,7 +7,7 @@ print ("what is your pet name? \n"  +  input(""))
 print ("your band name can be"+ " " + City_name + " " + pet_name)
 
 
-## Version 2
+### Version 2
 print("welcome to band name generator")
 city = input ("what the name of the city you grew up in? \n")
 pet = input("what is your pet name ? \n")
