52cd49158352aa7527ad3853fd6b1bae01bc622a
smusipat <mskrmanthani@gmail.com>
2024-10-01 22:40:33 -0400

    new code

---
+++ Day2.py
@@ -0,0 +1,53 @@
+#datatype
+print(type(34.34))
+print(type(False))
+print(type(1234567))
+print(type("hello"))
+print (len("shiva"));
+#subscripting
+len("shivareddy")
+print ("shivakumar"[-9])  #example 
+print("1234" + "334qer")
+print(123 + 44)
+print (2.445)
+print(True)
+print(False)
+
+#typecasting and datatype conversions 
+print(int("134") + int("876")) # to convert the data type called typecasting
+my_name = input("user name")
+lenght_of_name = len(my_name)
+print (type(my_name))
+print (type(lenght_of_name))
+print("number of letters in your name:" + str(lenght_of_name))
+
+
+#mathematical operations
+print("print my age :" + input("age"))
+print(12 + 23)
+print(564 - 12)
+print( 34 * 45)
+print( 34 / 45) 
+print(23 // 34)
+print(23 % 34)
+print(34 ** 2)
+print(3 * 3 + 3 /3-3)
+print(3 / 3 - 3 /3+3)
+print(3 / (3 + 3) /3-3)
+bmi= 84/1.67 **2
+print(bmi)
+print(int(bmi))
+print(round(bmi, 3))
+
+#fstring
+score=0
+height=3.6
+is_winning= True
+print(f"your score is {score}, your height is {height} and you are {is_winning}")
+
+
+
+
+
+
+
---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+
+print ("welcome to the tip caluculator!" )
+bill =float(input("what is your bill ? $" +""))
+print (f"your total bill is ? {bill}")
+tip= int(input("whats the peercentage of bill you woulde liem to give 10 12 15" +""))
+people= int( input("how many people had lunch"+ ""))
+bill_with_tip= tip /100 *bill + bill
+print (bill_with_tip)
+bill_per_person= bill_with_tip /people
+final_bill =round (bill_per_person, 2)
+print(f"each person need to pay: ${final_bill}")
