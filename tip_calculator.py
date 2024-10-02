2aaeabff5871599ee68f33bc5d9c9b54b280fac7
smusipat <mskrmanthani@gmail.com>
2024-10-01 22:43:04 -0400

    edited print

--- tip_calculator.py
+++ tip_calculator.py
@@ -5,7 +5,6 @@ print (f"your total bill is ? {bill}")
 tip= int(input("whats the peercentage of bill you woulde liem to give 10 12 15" +""))
 people= int( input("how many people had lunch"+ ""))
 bill_with_tip= tip /100 *bill + bill
-print (bill_with_tip)
 bill_per_person= bill_with_tip /people
 final_bill =round (bill_per_person, 2)
 print(f"each person need to pay: ${final_bill}")
